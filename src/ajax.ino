2de3e5451203bfb2a6258f043c01fc884a61ec1d
mrv96 <mrv96@users.noreply.github.com>
2024-07-18 20:49:34 +0200

    Restore other sources keeping ethernet

--- src/ajax.ino
+++ src/ajax.ino
@@ -1,10 +1,7 @@
 /*
-dualETH Ethernet ArtNet Node
-
-Base Code Copyright (c) 2016, Matthew Tong
-https://github.com/mtongnz/
-Ethernet Implementation Copyright (c) 2023, expanseElectronics Ltd
-https://github.com/expanseElectronics/
+ESP8266_ArtNetNode v2.0.0
+Copyright (c) 2016, Matthew Tong
+https://github.com/mtongnz/ESP8266_ArtNetNode_v2
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
 License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
@@ -22,6 +19,7 @@ void ajaxHandle() {
 
   String reply;
 
+  // Handle request to reboot into update mode
   if (json.containsKey("success") && json["success"] == 1 && json.containsKey("doUpdate")) {
     artRDM.end();
 
@@ -32,6 +30,9 @@ void ajaxHandle() {
     webServer.send(200, "application/json", reply);
 
     if (json["doUpdate"] == 1) {
+      // Turn pixel strips off if they're on
+      pixDriver.updateStrip(0, 0, deviceSettings.portApixConfig);
+      pixDriver.updateStrip(1, 0, deviceSettings.portBpixConfig);
 
       deviceSettings.doFirmwareUpdate = true;
       eepromSave();
@@ -39,6 +40,7 @@ void ajaxHandle() {
       doReboot = true;
     }
 
+  // Handle load and save of data
   } else if (json.containsKey("success") && json["success"] == 1 && json.containsKey("page")) {
     if (ajaxSave((uint8_t)json["page"], json)) {
       ajaxLoad((uint8_t)json["page"], jsonReply);
@@ -51,13 +53,18 @@ void ajaxHandle() {
       jsonReply["message"] = "Failed to save data.  Reload page and try again.";
     }
 
+  // Handle reboots
   } else if (json.containsKey("success") && json.containsKey("reboot") && json["reboot"] == 1) {
     jsonReply["success"] = 1;
     jsonReply["message"] = "Device Restarting.";
 
+    // Turn pixel strips off if they're on
+    pixDriver.updateStrip(0, 0, deviceSettings.portApixConfig);
+    pixDriver.updateStrip(1, 0, deviceSettings.portBpixConfig);
 
     doReboot = true;
 
+  // Handle errors
   }
 
   jsonReply.printTo(reply);
@@ -65,15 +72,17 @@ void ajaxHandle() {
 }
 
 bool ajaxSave(uint8_t page, JsonObject& json) {
+  // This is a load request, not a save
   if (json.size() == 2)
     return true;
 
   switch (page) {
-    case 1:
+    case 1:     // Device Status
+      // We don't need to save anything for this.  Go straight to load
       return true;
       break;
 
-    case 2:
+    case 2:     // Wifi
       json.get<String>("wifiSSID").toCharArray(deviceSettings.wifiSSID, 40);
       json.get<String>("wifiPass").toCharArray(deviceSettings.wifiPass, 40);
       json.get<String>("hotspotSSID").toCharArray(deviceSettings.hotspotSSID, 20);
@@ -85,20 +94,46 @@ bool ajaxSave(uint8_t page, JsonObject& json) {
       return true;
       break;
 
-    case 3:
+    case 3:     // IP Address & Node Name
       deviceSettings.ip = IPAddress(json["ipAddress"][0],json["ipAddress"][1],json["ipAddress"][2],json["ipAddress"][3]);
       deviceSettings.subnet = IPAddress(json["subAddress"][0],json["subAddress"][1],json["subAddress"][2],json["subAddress"][3]);
       deviceSettings.gateway = IPAddress(json["gwAddress"][0],json["gwAddress"][1],json["gwAddress"][2],json["gwAddress"][3]);
       deviceSettings.broadcast = uint32_t(deviceSettings.ip) | (~uint32_t(deviceSettings.subnet));
+      //deviceSettings.broadcast = {~deviceSettings.subnet[0] | (deviceSettings.ip[0] & deviceSettings.subnet[0]), ~deviceSettings.subnet[1] | (deviceSettings.ip[1] & deviceSettings.subnet[1]), ~deviceSettings.subnet[2] | (deviceSettings.ip[2] & deviceSettings.subnet[2]), ~deviceSettings.subnet[3] | (deviceSettings.ip[3] & deviceSettings.subnet[3])};
+
       json.get<String>("nodeName").toCharArray(deviceSettings.nodeName, 18);
       json.get<String>("longName").toCharArray(deviceSettings.longName, 64);
 
       if (!isHotspot && (bool)json["dhcpEnable"] != deviceSettings.dhcpEnable) {
 
         if ((bool)json["dhcpEnable"]) {
+          /*
+          // Re-enable DHCP
+          WiFi.begin(deviceSettings.wifiSSID, deviceSettings.wifiPass);
+
+          // Wait for an IP
+          while (WiFi.status() != WL_CONNECTED)
+            yield();
+
+          // Save settings to struct
+          deviceSettings.ip = WiFi.localIP();
+          deviceSettings.subnet = WiFi.subnetMask();
+          deviceSettings.broadcast = {~deviceSettings.subnet[0] | (deviceSettings.ip[0] & deviceSettings.subnet[0]), ~deviceSettings.subnet[1] | (deviceSettings.ip[1] & deviceSettings.subnet[1]), ~deviceSettings.subnet[2] | (deviceSettings.ip[2] & deviceSettings.subnet[2]), ~deviceSettings.subnet[3] | (deviceSettings.ip[3] & deviceSettings.subnet[3])};
+          */
+
           deviceSettings.gateway = INADDR_NONE;
 
         }
+        /*
+        else {
+          // Set static IP
+          WiFi.config(deviceSettings.ip,deviceSettings.ip,deviceSettings.ip,deviceSettings.subnet);
+        }
+
+        // Add any changes to artnet settings - this will send correct artpollreply
+        artRDM.setIP(deviceSettings.ip, deviceSettings.subnet);
+        artRDM.setDHCP(deviceSettings.dhcpEnable);
+        */
 
         doReboot = true;
       }
@@ -114,7 +149,7 @@ bool ajaxSave(uint8_t page, JsonObject& json) {
       return true;
       break;
 
-    case 4:
+    case 4:     // Port A
       {
         deviceSettings.portAprot = (uint8_t)json["portAprot"];
         bool e131 = (deviceSettings.portAprot == PROT_ARTNET_SACN) ? true : false;
@@ -143,6 +178,7 @@ bool ajaxSave(uint8_t page, JsonObject& json) {
         bool updatePorts = false;
 
         #ifndef ONE_PORT
+          // RDM and DMX input can't run together
           if (newMode == TYPE_DMX_IN && deviceSettings.portBmode == TYPE_RDM_OUT) {
             deviceSettings.portBmode = TYPE_DMX_OUT;
             dmxB.rdmDisable();
@@ -155,17 +191,74 @@ bool ajaxSave(uint8_t page, JsonObject& json) {
 
         if (newMode != oldMode) {
 
+          // Store the nem mode to settings
           deviceSettings.portAmode = newMode;
 
           doReboot = true;
+          /*
+          if (oldMode == TYPE_WS2812) {
+            doReboot = true;
+
+            // Set pixel strip length to zero
+            pixDriver.updateStrip(0, 0, deviceSettings.portApixConfig);
+
+            // Close ports from pixels - library handles if they dont exist
+            for (uint8_t x = 2; x <= 4; x++)
+              artRDM.closePort(portA[0], portA[x]);
+
+            // Start our DMX port
+            dmxA.begin(DMX_DIR_A, artRDM.getDMX(portA[0], portA[1]));
+
+          } else if (oldMode == TYPE_DMX_IN)
+            dmxA.dmxIn(false);
+          else if (oldMode == TYPE_RDM_OUT)
+            dmxA.rdmDisable();
+
+          // Start DMX output with no DMX
+          if (newMode == TYPE_DMX_OUT) {
+
+
+            artRDM.setPortType(portA[0], portA[1], DMX_OUT);
+
+          // Start DMX output with RDM
+          } else if (newMode == TYPE_RDM_OUT) {
+            dmxA.rdmEnable(ESTA_MAN, ESTA_DEV);
+            dmxA.rdmSetCallBack(rdmReceivedA);
+            dmxA.todSetCallBack(sendTodA);
+            artRDM.setPortType(portA[0], portA[1], RDM_OUT);
+
+          // Start DMX input
+          } else if (newMode == TYPE_DMX_IN) {
+            dmxA.dmxIn(true);
+            dmxA.setInputCallback(dmxIn);
+
+            artRDM.setPortType(portA[0], portA[1], DMX_IN);
+
+          // Start WS2812 output
+          } else if (newMode == TYPE_WS2812) {
+            doReboot = true;
+
+            dmxA.end();
+
+            artRDM.setPortType(portA[0], portA[1], TYPE_DMX_OUT);
+            updatePorts = true;
+
+            // Initialize the pixel strip
+            pixDriver.setStrip(0, DMX_TX_A, deviceSettings.portAnumPix, deviceSettings.portApixConfig);
+
+          }
+          */
         }
 
+        // Update the Artnet class
         artRDM.setNet(portA[0], deviceSettings.portAnet);
         artRDM.setSubNet(portA[0], deviceSettings.portAsub);
         artRDM.setUni(portA[0], portA[1], deviceSettings.portAuni[0]);
         artRDM.setMerge(portA[0], portA[1], deviceSettings.portAmerge);
 
+        // Lengthen or shorten our pixel strip & handle required Artnet ports
         if (newMode == TYPE_WS2812 && !doReboot) {
+          // Get the new & old lengths of pixel strip
           uint16_t newLen = (json.containsKey("portAnumPix")) ? (uint16_t)json["portAnumPix"] : deviceSettings.portAnumPix;
           if (newLen > 680)
             newLen = 680;
@@ -173,28 +266,38 @@ bool ajaxSave(uint8_t page, JsonObject& json) {
           uint16_t oldLen = deviceSettings.portAnumPix;
           bool lenChanged = false;
 
+          // If pixel size has changed
           if (newLen <= 680 && oldLen != newLen) {
+            // Update our pixel strip
             deviceSettings.portAnumPix = newLen;
+            pixDriver.updateStrip(1, deviceSettings.portAnumPix, deviceSettings.portApixConfig);
 
             lenChanged = true;
+
+            // If the old mode was pixel map then update the Artnet ports
             if (deviceSettings.portApixMode == FX_MODE_PIXEL_MAP)
               updatePorts = true;
           }
 
+          // If the old mode was 12 channel FX, update oldLen to represent the number of channels we used
           if (deviceSettings.portApixMode == FX_MODE_12)
             oldLen = 12;
 
+          // If our mode changes then update the Artnet ports
           if (deviceSettings.portApixMode != (uint8_t)json["portApixMode"])
             updatePorts = true;
 
+          // Store the new pixel mode
           deviceSettings.portApixMode = (uint8_t)json["portApixMode"];
 
+          // If our new mode is FX12 then we need 12 channels & store the start address
           if (deviceSettings.portApixMode == FX_MODE_12) {
             if ((uint16_t)json["portApixFXstart"] <= 501 && (uint16_t)json["portApixFXstart"] > 0)
               deviceSettings.portApixFXstart = (uint16_t)json["portApixFXstart"];
             newLen = 12;
           }
 
+          // If needed, open and close Artnet ports
           if (updatePorts) {
             for (uint8_t x = 1, y = 2; x < 4; x++, y++) {
               uint16_t c = (x * 170);
@@ -205,6 +308,7 @@ bool ajaxSave(uint8_t page, JsonObject& json) {
             }
           }
 
+          // Set universe and merge settings (port 1 is done above for all port types)
           for (uint8_t x = 1, y = 2; x < 4; x++, y++) {
             if (newLen > (x * 170)) {
               artRDM.setUni(portA[0], portA[y], deviceSettings.portAuni[x]);
@@ -220,7 +324,7 @@ bool ajaxSave(uint8_t page, JsonObject& json) {
       }
       break;
 
-    case 5:
+    case 5:     // Port B
       #ifndef ONE_PORT
       {
         deviceSettings.portBprot = (uint8_t)json["portBprot"];
@@ -249,23 +353,73 @@ bool ajaxSave(uint8_t page, JsonObject& json) {
         uint8_t oldMode = deviceSettings.portBmode;
         bool updatePorts = false;
 
+        // RDM and DMX input can't run together
         if (newMode == TYPE_RDM_OUT && deviceSettings.portAmode == TYPE_DMX_IN)
           newMode = TYPE_DMX_OUT;
 
         if (newMode != oldMode) {
 
+          // Store the nem mode to settings
           deviceSettings.portBmode = newMode;
 
           doReboot = true;
 
+          /*
+          if (oldMode == TYPE_WS2812) {
+            doReboot = true;
+
+            // Set pixel strip length to zero
+            pixDriver.updateStrip(1, 0, deviceSettings.portBpixConfig);
+
+            // Close ports from pixels - library handles if they dont exist
+            for (uint8_t x = 2; x <= 4; x++)
+              artRDM.closePort(portB[0], portB[x]);
+
+            // Start our DMX port
+            dmxB.begin(DMX_DIR_B, artRDM.getDMX(portB[0], portB[1]));
+
+
+          } else if (oldMode == TYPE_RDM_OUT)
+            dmxB.rdmDisable();
+
+
+
+          // Start DMX output with no DMX
+          if (newMode == TYPE_DMX_OUT) {
+            artRDM.setPortType(portB[0], portB[1], DMX_OUT);
+
+          // Start DMX output with RDM
+          } else if (newMode == TYPE_RDM_OUT) {
+            dmxB.rdmEnable(ESTA_MAN, ESTA_DEV);
+            dmxB.rdmSetCallBack(rdmReceivedB);
+            dmxB.todSetCallBack(sendTodB);
+            artRDM.setPortType(portB[0], portB[1], RDM_OUT);
+
+          // Start WS2812 output
+          } else if (newMode == TYPE_WS2812) {
+            doReboot = true;
+
+
+            //dmxB.end();
+            artRDM.setPortType(portB[0], portB[1], TYPE_DMX_OUT);
+            updatePorts = true;
+
+            // Initialize the pixel strip
+            pixDriver.setStrip(1, DMX_TX_B, deviceSettings.portBnumPix, deviceSettings.portBpixConfig);
+
+          }
+          */
         }
 
+        // Update the Artnet class
         artRDM.setNet(portB[0], deviceSettings.portBnet);
         artRDM.setSubNet(portB[0], deviceSettings.portBsub);
         artRDM.setUni(portB[0], portB[1], deviceSettings.portBuni[0]);
         artRDM.setMerge(portB[0], portB[1], deviceSettings.portBmerge);
 
+        // Lengthen or shorten our pixel strip & handle required Artnet ports
         if (newMode == TYPE_WS2812 && !doReboot) {
+          // Get the new & old lengths of pixel strip
           uint16_t newLen = (json.containsKey("portBnumPix")) ? (uint16_t)json["portBnumPix"] : deviceSettings.portBnumPix;
           if (newLen > 680)
             newLen = 680;
@@ -273,28 +427,38 @@ bool ajaxSave(uint8_t page, JsonObject& json) {
           uint16_t oldLen = deviceSettings.portBnumPix;
           bool lenChanged = false;
 
+          // If pixel size has changed
           if (newLen <= 680 && oldLen != newLen) {
+            // Update our pixel strip
             deviceSettings.portBnumPix = newLen;
+            pixDriver.updateStrip(1, deviceSettings.portBnumPix, deviceSettings.portBpixConfig);
 
             lenChanged = true;
+
+            // If the old mode was pixel map then update the Artnet ports
             if (deviceSettings.portBpixMode == FX_MODE_PIXEL_MAP)
               updatePorts = true;
           }
 
+          // If the old mode was 12 channel FX, update oldLen to represent the number of channels we used
           if (deviceSettings.portBpixMode == FX_MODE_12)
             oldLen = 12;
 
+          // If our mode changes then update the Artnet ports
           if (deviceSettings.portBpixMode != (uint8_t)json["portBpixMode"])
             updatePorts = true;
 
+          // Store the new pixel mode
           deviceSettings.portBpixMode = (uint8_t)json["portBpixMode"];
 
+          // If our new mode is FX12 then we need 12 channels & store the start address
           if (deviceSettings.portBpixMode == FX_MODE_12) {
             if ((uint16_t)json["portBpixFXstart"] <= 501 && (uint16_t)json["portBpixFXstart"] > 0)
               deviceSettings.portBpixFXstart = (uint16_t)json["portBpixFXstart"];
             newLen = 12;
           }
 
+          // If needed, open and close Artnet ports
           if (updatePorts) {
             for (uint8_t x = 1, y = 2; x < 4; x++, y++) {
               uint16_t c = (x * 170);
@@ -305,6 +469,7 @@ bool ajaxSave(uint8_t page, JsonObject& json) {
             }
           }
 
+          // Set universe and merge settings (port 1 is done above for all port types)
           for (uint8_t x = 1, y = 2; x < 4; x++, y++) {
             if (newLen > (x * 170)) {
               artRDM.setUni(portB[0], portB[y], deviceSettings.portBuni[x]);
@@ -321,22 +486,26 @@ bool ajaxSave(uint8_t page, JsonObject& json) {
       #endif
       break;
 
-    case 6:
+    case 6:     // Scenes
+      // Not yet implemented
 
       return true;
       break;
 
-    case 7:
+    case 7:     // Firmware
+      // Doesn't come here
 
       break;
 
     default:
+      // Catch errors
       return false;
   }
 }
 
 void ajaxLoad(uint8_t page, JsonObject& jsonReply) {
 
+  // Create the needed arrays here - doesn't work within the switch below
   JsonArray& ipAddress = jsonReply.createNestedArray("ipAddress");
   JsonArray& subAddress = jsonReply.createNestedArray("subAddress");
   JsonArray& gwAddress = jsonReply.createNestedArray("gwAddress");
@@ -347,15 +516,16 @@ void ajaxLoad(uint8_t page, JsonObject& jsonReply) {
   JsonArray& portBsACNuni = jsonReply.createNestedArray("portBsACNuni");
   JsonArray& dmxInBroadcast = jsonReply.createNestedArray("dmxInBroadcast");
 
+  // Get MAC Address
   char MAC_char[30] = "";
-  sprintf(MAC_char, "%02X", mac[0]);
+  sprintf(MAC_char, "%02X", MAC_array[0]);
   for (int i = 1; i < 6; ++i)
-    sprintf(MAC_char, "%s:%02X", MAC_char, mac[i]);
+    sprintf(MAC_char, "%s:%02X", MAC_char, MAC_array[i]);
 
   jsonReply["macAddress"] = String(MAC_char);
 
   switch (page) {
-    case 1:
+    case 1:     // Device Status
       jsonReply.remove("ipAddress");
       jsonReply.remove("subAddress");
       jsonReply.remove("gwAddress");
@@ -423,7 +593,7 @@ void ajaxLoad(uint8_t page, JsonObject& jsonReply) {
       jsonReply["success"] = 1;
       break;
 
-    case 2:
+    case 2:     // Wifi
       jsonReply.remove("ipAddress");
       jsonReply.remove("subAddress");
       jsonReply.remove("gwAddress");
@@ -444,7 +614,7 @@ void ajaxLoad(uint8_t page, JsonObject& jsonReply) {
       jsonReply["success"] = 1;
       break;
 
-    case 3:
+    case 3:     // IP Address & Node Name
       jsonReply.remove("portAuni");
       jsonReply.remove("portBuni");
       jsonReply.remove("portAsACNuni");
@@ -466,7 +636,7 @@ void ajaxLoad(uint8_t page, JsonObject& jsonReply) {
       jsonReply["success"] = 1;
       break;
 
-    case 4:
+    case 4:     // Port A
       jsonReply.remove("ipAddress");
       jsonReply.remove("subAddress");
       jsonReply.remove("gwAddress");
@@ -476,6 +646,7 @@ void ajaxLoad(uint8_t page, JsonObject& jsonReply) {
 
       jsonReply["portAmode"] = deviceSettings.portAmode;
 
+      // Only Artnet supported for receiving right now
       if (deviceSettings.portAmode == TYPE_DMX_IN)
         jsonReply["portAprot"] = PROT_ARTNET;
       else
@@ -498,7 +669,7 @@ void ajaxLoad(uint8_t page, JsonObject& jsonReply) {
       jsonReply["success"] = 1;
       break;
 
-    case 5:
+    case 5:     // Port B
       jsonReply.remove("ipAddress");
       jsonReply.remove("subAddress");
       jsonReply.remove("gwAddress");
@@ -525,7 +696,7 @@ void ajaxLoad(uint8_t page, JsonObject& jsonReply) {
       jsonReply["success"] = 1;
       break;
 
-    case 6:
+    case 6:     // Scenes
       jsonReply.remove("ipAddress");
       jsonReply.remove("subAddress");
       jsonReply.remove("gwAddress");
@@ -540,7 +711,7 @@ void ajaxLoad(uint8_t page, JsonObject& jsonReply) {
       jsonReply["success"] = 1;
       break;
 
-    case 7:
+    case 7:     // Firmware
       jsonReply.remove("ipAddress");
       jsonReply.remove("subAddress");
       jsonReply.remove("gwAddress");
--- src/firmUpdate.ino
+++ src/firmUpdate.ino
@@ -1,10 +1,7 @@
 /*
-dualETH Ethernet ArtNet Node
-
-Base Code Copyright (c) 2016, Matthew Tong
-https://github.com/mtongnz/
-Ethernet Implementation Copyright (c) 2023, expanseElectronics Ltd
-https://github.com/expanseElectronics/
+ESP8266_ArtNetNode v2.0.0
+Copyright (c) 2016, Matthew Tong
+https://github.com/mtongnz/ESP8266_ArtNetNode_v2
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
 License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
@@ -16,10 +13,17 @@ You should have received a copy of the GNU General Public License along with thi
 If not, see http://www.gnu.org/licenses/
 */
 
+
+
+/* webFirmwareUpdate()
+ *  display update status after firmware upload and restart
+ */
 void webFirmwareUpdate() {
+  // Generate the webpage from the variables above
   String fail = "{\"success\":0,\"message\":\"Unknown Error\"}";
   String ok = "{\"success\":1,\"message\":\"Success: Device restarting\"}";
 
+  // Send to the client
   webServer.sendHeader("Connection", "close");
   webServer.sendHeader("Access-Control-Allow-Origin", "*");
   webServer.send(200, "application/json", (Update.hasError()) ? fail : ok);
@@ -27,6 +31,11 @@ void webFirmwareUpdate() {
   doReboot = true;
 }
 
+
+
+/* webFirmwareUpload()
+ *  handle firmware upload and update
+ */
 void webFirmwareUpload() {
   String reply = "";
   ethernetHTTPUpload& upload = webServer.upload();
@@ -42,7 +51,7 @@ void webFirmwareUpload() {
     }
 
   } else if(upload.status == UPLOAD_FILE_END){
-    if(Update.end(true)){
+    if(Update.end(true)){ //true to set the size to the current progress
       reply = "{\"success\":1,\"message\":\"Success: Device Restarting\"}";
     } else {
       reply = "{\"success\":0,\"message\":\"Unknown Error\"}";
@@ -50,6 +59,7 @@ void webFirmwareUpload() {
   }
   yield();
 
+  // Send to the client
   if (reply.length() > 0) {
     webServer.sendHeader("Connection", "close");
     webServer.sendHeader("Access-Control-Allow-Origin", "*");
